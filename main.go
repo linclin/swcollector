d25dd5ac86c24bef3353c254b65ead0de35dd257
迪安 <dian@juanpi.com>
2018-05-04 18:22:00 +0800

    test

--- main.go
+++ main.go
@@ -9,10 +9,12 @@ import (
 	"github.com/gaochao1/swcollector/funcs"
 	"github.com/gaochao1/swcollector/g"
 	"github.com/gaochao1/swcollector/http"
+	"github.com/astaxie/beego/orm"
 )
 
-func main() {
 
+
+func init() {
 	cfg := flag.String("c", "cfg.json", "configuration file")
 	version := flag.Bool("v", false, "show version")
 	check := flag.Bool("check", false, "check collector")
@@ -23,15 +25,9 @@ func main() {
 		fmt.Println(g.VERSION)
 		os.Exit(0)
 	}
+
 	g.ParseConfig(*cfg)
-	if g.Config().SwitchHosts.Enabled {
-		hostcfg := g.Config().SwitchHosts.Hosts
-		g.ParseHostConfig(hostcfg)
-	}
-	if g.Config().CustomMetrics.Enabled {
-		custMetrics := g.Config().CustomMetrics.Template
-		g.ParseCustConfig(custMetrics)
-	}
+
 	g.InitRootDir()
 	g.InitLocalIps()
 	g.InitRpcClients()
@@ -41,13 +37,27 @@ func main() {
 		os.Exit(0)
 	}
 
-	funcs.NewLastifMap()
-	funcs.BuildMappers()
+	dbUser := g.Config().CMDB.User
+	dbPass := g.Config().CMDB.Password
+	dbHost := g.Config().CMDB.Host
+	dbPort := g.Config().CMDB.Port
+	dbName := g.Config().CMDB.DB
+	maxIdleConn := 30
+	maxOpenConn := 100
+	dbLink := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", dbUser, dbPass, dbHost, dbPort, dbName) + "&loc=Asia%2FShanghai"
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.RegisterDataBase("default", "mysql", dbLink, maxIdleConn, maxOpenConn)
+	orm.Debug = false
+}
 
+func main() {
+	funcs.BuildMappers()
 	cron.Collect()
+	cron.CMDBReport()
 
 	go http.Start()
 
 	select {}
 
 }
+
